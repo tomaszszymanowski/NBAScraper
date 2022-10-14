60c8e5edd832575e35402e015534345fe18d4fd9
Tomasz <tomasz.szymanowski@gmail.com>
2022-10-14 17:45:41 +0200

    in progres

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,61 +3,69 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.progsport.com/").get();
-        //String title = doc.title();
+        /*String[] arr = element.split("\\s{2}");
+        String[] arr2 = element1.split("\\s{2}");
 
-        //Element element = doc.getElementById("anyid");
-        //Elements element = doc.getElementsByClass("F2");
+
+        List<String> listOfNumbers = new ArrayList<>(60);
+
+
+
+        for (String s : list) {
+            listOfNumbers.add(Main2.getNumbersFromString(s));
+        }
+
+        for (String st : listOfNumbers) {
+            System.out.println(st);
+        }*/
+        Document doc = Jsoup.connect("https://www.progsport.com/basketball/bsk-predictions-211111.html").get();
+
+//        Elements element = doc.getElementsByClass("F2");
+//
+//        System.out.println(element);
 
         String element = doc.getElementsByClass("F2").text();
         String element1 = doc.getElementsByClass("F1").text();
-
-
-//        System.out.println(elementStr);
-
-        String[] arr = element.split("  ");
-        String[] arr2 = element1.split("  ");
+        //System.out.println(element);
 
         List<String> list = new ArrayList<>(60);
+        List<String> list2 = new ArrayList<>(60);
 
-        for(String s : arr){
-            list.add(s);
+        String[] arr = element.split("NBA ");
+        String[] arr2 = element1.split("NBA ");
+
+        list.addAll(Arrays.asList(arr));
+        list.remove(0);
+
+        list.addAll(Arrays.asList(arr2));
+        list.remove(0);
+
+
+        //list.remove(0);
+        for (String s : list) {
+            if(s.length() < 500){
+                list2.add(s);
+                System.out.print(s.length() + ",");
+            }
         }
 
-        for(String s : arr2){
-            list.add(s);
-        }
-
-        for(String s : list){
+        for(String s : list2){
             System.out.println(s);
         }
 
-        /*String input = element2;
-        Pattern pattern = Pattern.compile("\\d+\\s");
-        Pattern pattern2 = Pattern.compile("\\d\\d\\d\\s");
-
-        Matcher m = pattern.matcher(input);
-        Matcher m2 = pattern2.matcher(input);
-        StringBuilder b = new StringBuilder();
 
 
-
-        while(m.find()) {
-            if(m2.find()){
-                b.append("\n");
-            }
-            b.append(m.group(0));
-        }
-        System.out.println(b);*/
     }
 
-    }
+}
 
--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,7 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main2 {
     public static void main(String[] args) {
-        String str = "AR1 Regatas Corrientes - Instituto AC Cordoba 50 50 2.23 1.58 4.5 61 39 167.0 48 52 168 Regatas Corrientes - Instituto AC Cordoba";
+        String str = "NBP Milwaukee Bucks - Brooklyn Nets 55 45 1.60 2.40 -4.5 42 58 229.0 38 62 234 97-107 Milwaukee Bucks - Brooklyn Nets 97-107 NBP Phoenix Suns - Sacramento Kings 71 29 1.40 3.00 -6.5 59 41 217.0 28 72 226 104-105 Phoenix Suns - Sacramento Kings 104-105";
+        //getNumbersFromString(str);
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.indexOf("NBP",50));
+        System.out.println(sb.subSequence(0, 124));
+    }
 
-        System.out.println(str.replaceAll("(?<=[a-z\\s])\\.", ""));
+    public static String getNumbersFromString(String str) {
+        StringBuilder sb = new StringBuilder();
+        String mydata = str;
+        Pattern pattern = Pattern.compile("([0-9.-]{2,}\\s)");
+        Matcher matcher = pattern.matcher(mydata);
+        while (matcher.find())
+        {
+            sb.append(matcher.group(1));
+        }
+        return sb.toString();
     }
 }
