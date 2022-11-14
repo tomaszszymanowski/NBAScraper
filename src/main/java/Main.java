e22e5efcacd1a869febf8e2efe7d123a51f54f99
Tomasz <tomasz.szymanowski@gmail.com>
2022-11-14 15:42:37 +0100

    download raw data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.awt.image.AreaAveragingScaleFilter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,55 +17,91 @@ public class Main {
     public static void main(String[] args) throws IOException {
         /*String[] arr = element.split("\\s{2}");
         String[] arr2 = element1.split("\\s{2}");
-
-
         List<String> listOfNumbers = new ArrayList<>(60);
-
-
-
         for (String s : list) {
             listOfNumbers.add(Main2.getNumbersFromString(s));
         }
-
         for (String st : listOfNumbers) {
             System.out.println(st);
         }*/
-        Document doc = Jsoup.connect("https://www.progsport.com/basketball/bsk-predictions-211111.html").get();
 
-//        Elements element = doc.getElementsByClass("F2");
+
+        Document doc = Jsoup.connect("https://www.progsport.com/basketball/bsk-predictions-221112.html").get();
+        Elements elements = doc.getElementsByTag("html");
+
+//        System.out.println(elements.get(0));
+        StringBuilder sb = new StringBuilder(String.valueOf(elements.get(0).toString().replaceAll("><", ">0<").replaceAll("> <", ">0<")));
+
+        //System.out.println(sb);
+        String html = sb.toString().replaceAll("00", "0");
+        doc = Jsoup.parse(html);
+
+        elements = doc.getElementsByClass("F2");
+
+
+        System.out.println(elements.size());
+        int index = elements.size();
+        int count = 0;
+        while (count < index){
+            System.out.println(elements.get(count).text().replaceAll("00 0", "").replaceAll("00 ", "").replaceFirst("0", ""));
+            count += 3;
+        }
+
+        Elements elements1 = doc.getElementsByClass("F1");
+        System.out.println(elements1.size());
+        int index1 = elements.size();
+        int count1 = 0;
+        while (count1 < index1){
+            System.out.println(elements.get(count1).text().replaceAll("00 0", "").replaceAll("00 ", "").replaceFirst("0", ";"));
+            count1 += 3;
+        }
+
+//        try {
+//            File myObj = new File("filename.txt");
+//            if (myObj.createNewFile()) {
+//                System.out.println("File created: " + myObj.getName());
+//            } else {
+//                System.out.println("File already exists.");
+//            }
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
 //
-//        System.out.println(element);
+//        try {
+//            FileWriter myWriter = new FileWriter("filename.txt");
+//            myWriter.write(str);
+//            myWriter.close();
+//            System.out.println("Successfully wrote to the file.");
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
 
-        String element = doc.getElementsByClass("F2").text();
-        String element1 = doc.getElementsByClass("F1").text();
-        //System.out.println(element);
+//        List<String> list = new ArrayList<>(60);
+//        List<String> list2 = new ArrayList<>(60);
 
-        List<String> list = new ArrayList<>(60);
-        List<String> list2 = new ArrayList<>(60);
-
-        String[] arr = element.split("NBA ");
-        String[] arr2 = element1.split("NBA ");
-
-        list.addAll(Arrays.asList(arr));
-        list.remove(0);
-
-        list.addAll(Arrays.asList(arr2));
-        list.remove(0);
-
-
-        //list.remove(0);
-        for (String s : list) {
-            if(s.length() < 500){
-                list2.add(s);
-                System.out.print(s.length() + ",");
-            }
-        }
-
-        for(String s : list2){
-            System.out.println(s);
-        }
-
-
+//        String[] arr = element.split(" ");
+//        String[] arr2 = element1.split(" ");
+//
+//        list.addAll(Arrays.asList(arr));
+//        list.remove(0);
+//
+//        list.addAll(Arrays.asList(arr2));
+//        list.remove(0);
+//
+//
+//        //list.remove(0);
+//        for (String s : list) {
+//            if(s.length() < 500){
+//                list2.add(s);
+//                System.out.print(s.length() + ",");
+//            }
+//        }
+//
+//        for(String s : list2){
+//            System.out.println(s);
+//        }
 
     }
 
